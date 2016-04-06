e8143fea814d8b558cba8b7731efd8c7d56a90c6
catarak <ctarakajian@gmail.com>
2016-04-06 17:12:32 -0400

    add sections, logo, lots of other things

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function() {
+  $("#about").click(function(e) {
+		e.preventDefault();
+		$("html, body").animate({ scrollTop: $('#about-section').offset().top }, 500);
+  });
+
+  $("#purchase").click(function(e) {
+		e.preventDefault();
+		$("html, body").animate({ scrollTop: $('#purchase-section').offset().top }, 500);
+  });
+
+  $("#logo").click(function(e) {
+		e.preventDefault();
+		$("html, body").animate({scrollTop: 0}, 500);
+  });
+});
