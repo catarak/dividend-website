628e43d08f4448996dad2fa9f5689dc9b1ca660f
catarak <ctarakajian@gmail.com>
2016-04-14 19:19:15 -0400

    fix copy and styling

--- transaction.sh
+++ transaction.sh
@@ -13,4 +13,4 @@ while getopts "d:" arg; do
 done
 
 
-counterparty-client --wallet-password="escape" send --source=191A1Uq7xftJu9yisYYQe6pykA9k1EQnNE --quantity=1 --asset=CASSIE --destination=$destination
+counterparty-client --wallet-password="escape" send --source=191A1Uq7xftJu9yisYYQe6pykA9k1EQnNE --quantity=1 --asset=DIVIDENDPLUS --destination=$destination
